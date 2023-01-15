c053890546fd3d800200ee63e1bebc1846c770e5
Pranjal Yadav <2019uec1008@mnit.ac.in>
2023-01-15 23:28:08 +0530

    ✨ add: new features

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { UserSearchComponent } from './components/user-search/user-search.component';
+
+import { NgZorroAntdModule } from './antd-module/ng-zorro-antd.module';
+import { UserDetailsComponent } from './components/user-details/user-details.component';
+import { RepoDetailsComponent } from './components/repo-details/repo-details.component';
+import { RepoListComponent } from './components/repo-list/repo-list.component';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { LoaderComponent } from './components/loader/loader.component';
+import { ErrorComponent } from './components/error/error.component';
+import { EmptyComponent } from './components/empty/empty.component';
+
+registerLocaleData(en);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, UserSearchComponent, UserDetailsComponent, RepoDetailsComponent, RepoListComponent, PaginationComponent, LoaderComponent, ErrorComponent, EmptyComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NgZorroAntdModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
