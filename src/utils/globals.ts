f910cfdfeb9e6424ab5cca225f933b45099972c8
Pranjal Yadav <2019uec1008@mnit.ac.in>
2023-01-16 06:37:32 +0530

    minor changes

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = process.env["backend"] || "http://localhost:5000";
+export const BACKEND_URL = "https://fyle-backend-production.up.railway.app/";
 export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_PAGE_NUMBER = 1;
