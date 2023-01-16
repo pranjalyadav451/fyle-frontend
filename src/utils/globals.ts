3ed560c8e743bcd1f3f2e0c7cc168f62e52f9ded
Pranjal Yadav <2019uec1008@mnit.ac.in>
2023-01-16 06:23:58 +0530

    🚀

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,3 +1,3 @@
-export const BACKEND_URL = "http://localhost:5000";
+export const BACKEND_URL = process.env["backend"] || "http://localhost:5000";
 export const DEFAULT_PAGE_SIZE = 10;
 export const DEFAULT_PAGE_NUMBER = 1;
